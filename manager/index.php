07ae4308da78ebb10b77f8ac735e9b67807d5715
Pravin Adhav <pravinadhav4945@gmail.com>
2022-11-26 21:48:02 +0530

    added log out option to platform

--- manager/index.php
+++ manager/index.php
@@ -16,6 +16,7 @@ include("../config/essential.php");
         <h4>name   :  <?php echo $_SESSION['fname'] ?></h4>
         <h4>lname  :  <?php echo $_SESSION['lname'] ?></h4>
         <h4>role   :  <?php echo $_SESSION['role'] ?></h4>
+        <a href="../config/logout.php"><button>Log Out</button></a>
     </center>
 </body>
 </html>
