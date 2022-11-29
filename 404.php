afc1cb79d1db1950a6bf5e371da6f033fe5e7bf2
Pravin Adhav <pravinadhav4945@gmail.com>
2022-11-29 13:04:12 +0530

    404 error page

---
+++ 404.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error-404</title>
+</head>
+<style>
+    body{
+        height: 100vh;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+    }
+    h2{
+        color: red;
+
+    }
+</style>
+<body>
+    <center>
+        <h3>Page Not found</h3>
+    </center>
+    <center>
+        <h4>We could not find the above page on our servers.</h4>
+    </center>
+    <center>
+        <h2>404</h2>
+    </center>
+</body>
+
+</html>
