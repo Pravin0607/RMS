7720b06db9c600d6abb972de5745fd08d90112f5
Pravin Adhav <pravinadhav4945@gmail.com>
2022-12-08 09:44:50 +0530

    changing contents from the essential file

--- config/essential.php
+++ config/essential.php
@@ -1,12 +1,6 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 <?php
 session_start();
-if(isset($_SESSION['role']))
-{
-    echo "True";
-}
-else
-{
+if(!isset($_SESSION['role']))
     header("location:/RMS");
-}
 ?>
