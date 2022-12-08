480dc25c60c0fd6d2f7604d3a56ca00fd96bb7b5
Pravin Adhav <pravinadhav4945@gmail.com>
2022-12-08 09:42:34 +0530

    changing some content in the manager index file

--- manager/index.php
+++ manager/index.php
@@ -11,11 +11,7 @@ include("../config/essential.php");
 </head>
 <body>
     <center>
-        <h3>Welcome Manager</h3>
-        <h4>userid :  <?php echo $_SESSION['userid'] ?></h4>
-        <h4>name   :  <?php echo $_SESSION['fname'] ?></h4>
-        <h4>lname  :  <?php echo $_SESSION['lname'] ?></h4>
-        <h4>role   :  <?php echo $_SESSION['role'] ?></h4>
+        <h3>Welcome <b><?php echo $_SESSION['fname'] ?></b></h3>
         <a href="../config/logout.php"><button>Log Out</button></a>
     </center>
 </body>
